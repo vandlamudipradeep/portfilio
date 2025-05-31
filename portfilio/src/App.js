415794e6310c7c7b3469f08f1f49a4e5d3866d43
vadlamudi pradeep <98800626+vandlamudipradeep@users.noreply.github.com>
2025-05-31 15:25:49 +0530

    Update App.js

--- portfilio/src/App.js
+++ portfilio/src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Headers';
 import About from './components/About';
 import Education from './components/Education';
